a238741ec292bd21cd16c1d1338943f8e47efa5f
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-12 23:05:48 +0600

    create a new collection and data get with email

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const jobCollection = client.db("job-portal").collection("jobs");
+    const jobApplicationCollection = client
+      .db("job-portal")
+      .collection("jobApplication");
 
     app.get("/jobs", async (req, res) => {
       const cursor = jobCollection.find();
@@ -32,12 +35,53 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/jobs/:id', async (req, res) => {
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const quarry = { _id: new ObjectId(id) };
       const result = await jobCollection.findOne(quarry);
       res.send(result);
     });
+
+    // job Application
+
+    app.get("/job-application", async (req, res) => {
+      const email = req.query.email;
+      const query = { user_email: email };
+      const result = await jobApplicationCollection.find(query).toArray();
+      for (const application of result) {
+        const query1 = { _id: new ObjectId(application.user_id) };
+        const job = await jobCollection.findOne(query1);
+
+        if (job) {
+          (application.title = job.title), (application.company = job.company);
+          application.company_logo = job.company_logo;
+          application.location = job.location;
+          application.jobType = job.jobType;
+          application.applicationDeadline = job.applicationDeadline;
+        }
+      }
+      res.send(result);
+    });
+
+    app.post("/job-application", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
+
+    app.get("/job-application/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobApplicationCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/job-application/:id", async (req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobApplicationCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
